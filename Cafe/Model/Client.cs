3b48a1d809a3093e5aa79cad4634c27f7569ab14
st403 <st403@WS403-14>
2026-02-16 09:28:58 +0300

    Создан класс Client

---
+++ Cafe/Model/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace Cafe.Model
+{
+    public class Client : EFModel           //Клиент
+    {                                       //
+        public string FullName { get; set; }//ФИО
+        public string? Email { get; set; }  //Почта
+        public string? Phone {  get; set; } //Номер телефона
+        public int? Age { get; set; }       //Возвраст
+    }
+}
