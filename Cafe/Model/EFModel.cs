c3ab26c2b3c4ff2d370a7e57ba02cf4fcf38fb66
st403 <st403@WS403-14>
2026-02-16 09:20:22 +0300

    Созданы классы EFModel и Worker

---
+++ Cafe/Model/EFModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Cafe.Model
+{
+    public class EFModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+
+    }
+}
---
+++ Cafe/Model/Worker.cs
@@ -0,0 +1,10 @@
+﻿namespace Cafe.Model
+{
+    public class Worker : EFModel               //Работник
+    {                                           //
+        public string FullName { get; set; }    //ФИО
+        public int Age { get; set; }            //Возвраст
+        public string Position { get; set; }    //Должность
+        public string Phone { get; set; }       //Номер телефона
+    }
+}
