93c24f7aee90b805bdf0424813d63af78d44cf43
VadimKuvaldaev <vadimkuvaldaev@gmail.com>
2026-03-30 09:16:07 +0300

    Добавлена страница с списком клиентов

--- Cafe/Model/Order.cs
+++ Cafe/Model/Order.cs
@@ -1,16 +1,16 @@
 ﻿namespace Cafe.Model
 {
-    public class Order : EFModel                    //Заказ
-    {                                               //
-        public int NumberOrder { get; set; }        //Номер заказа
-        public List<Meal> Contains { get; set; }    //Содержимое заказа
-        public double TotalAmount { get; set; }     //Счет
-        public string Status { get; set; }          //Статус
-                                                    //Конструктор заказа (снизу)
-        public Order(int numberOrder, List<Meal> contains, double totalAmount, string status)
+    public class Order : EFModel                             //Заказ
+    {                                                        //
+        public int NumberOrder { get; set; }                 //Номер заказа
+        //public List<Meal> Contains { get; set; } = new();    //Содержимое заказа
+        public double TotalAmount { get; set; }              //Счет
+        public string Status { get; set; }                   //Статус
+                                                             //Конструктор заказа (снизу)
+        public Order(int numberOrder, /*List<Meal> contains, */double totalAmount, string status)
         {
             NumberOrder = numberOrder;
-            Contains = contains;
+            //Contains = contains;
             TotalAmount = totalAmount;
             Status = status;
         }
---
+++ Cafe/Pages/Objects/Clients/ClientsList.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Cafe.Data;
+using Cafe.Model;
+
+namespace Cafe.Pages.Objects.Clients
+{
+    public class ClientsListModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientsListModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public List<Client> ClientsList { get; set; }
+
+        public void OnGet()
+        {
+            ClientsList = _context.Clients.ToList();
+        }
+    }
+}
