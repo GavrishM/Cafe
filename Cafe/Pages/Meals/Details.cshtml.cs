9672fcba358c0871f3ef5885ce2513ce0e326ef1
VadimKuvaldaev <vadimkuvaldaev@gmail.com>
2026-04-11 13:24:20 +0300

    Реализованы страницы "инфо"

--- Cafe/Pages/Meals/Details.cshtml.cs
+++ Cafe/Pages/Meals/Details.cshtml.cs
@@ -1,12 +1,32 @@
+using Cafe.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cafe.Pages.Meals
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public DetailsModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public Cafe.Model.Meal Meal { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Meal = await _context.Meals
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (Meal == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
         }
     }
 }
